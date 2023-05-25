5b42837decf9c1c9d565ef905e0fa4d8b357670a
Masato Ota <heiwa4126@gmail.com>
2023-05-25 14:29:11 +0900

    update

---
+++ README.md
@@ -0,0 +1,6 @@
+# vite-mui
+
+ReactでMUI(Material UI)の練習。
+
+[Install &amp; Setup Vite + React + Typescript + MUI 5 - Frontendshape](https://frontendshape.com/post/install-setup-vite-react-typescript-mui-5)
+をやってみた。
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: process.env.GITHUB_REPO_NAME ?? "./",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          r: ["react", "react-dom"],
+          m: ["@emotion/react", "@emotion/styled", "@mui/material"],
+        },
+      },
+    },
+  },
+});

78b830b4dfae38e7ac7d087e30a400a03503b3af
Masato Ota <heiwa4126@gmail.com>
2023-05-25 16:06:03 +0900

    update

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ReactでMUI (Material UI) の練習として
 
 - [Install &amp; Setup Vite + React + Typescript + MUI 5 - Frontendshape](https://frontendshape.com/post/install-setup-vite-react-typescript-mui-5)
+- [Installation - Material UI](https://mui.com/material-ui/getting-started/installation/)
+- [Vite+React+TypeScript で、UIフレームワークにMUI(v5)を導入する。](https://zenn.dev/longbridge/articles/bba17785710c1a)
+- [MUI v5 Theme ～基本の使い方からカスタマイズまで～](https://zenn.dev/longbridge/articles/c100d0311ed1be)
 
 をやってみた。
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
+import "@fontsource/noto-sans-jp/300.css";
+import "@fontsource/noto-sans-jp/400.css";
+import "@fontsource/noto-sans-jp/500.css";
+import "@fontsource/noto-sans-jp/700.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import CssBaseline from "@mui/material/CssBaseline";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import CssBaseline from "@mui/material/CssBaseline";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
